1805c3aaa08f40cc6dc6116c748eb89f6cdcb5c3
blainealbert <43224830+blainealbert@users.noreply.github.com>
2023-04-26 04:11:51 +0000

    fix: fixing errors for vercel build

--- client/src/components/CityInfo.js
+++ client/src/components/CityInfo.js
@@ -1,5 +1,3 @@
-import {useState} from 'react';
-
 function CityInfo(props) {
     // const [c]
 
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return (
       <footer>
         <div>
-            <p>React app by Blaine Albert using <a href="https://aqicn.org/" target="_blank">Air Quality Open Data Platform</a></p>
+            <p>React app by Blaine Albert using <a href="https://aqicn.org/" target="_blank" rel="noreferrer">Air Quality Open Data Platform</a></p>
         </div>
       </footer>
   );
